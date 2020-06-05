e461ed09f9a77d31aa906ae2cfe802717408f110
7ashow <7a.show@gmail.com>
2020-06-06 01:58:12 +0900

    add execution mining in threads

--- blockchain.py
+++ blockchain.py
@@ -1,8 +1,10 @@
+import contextlib
 import hashlib
 import json
 import logging
 import sys
 import time
+import threading
 
 from ecdsa import NIST256p
 from ecdsa import VerifyingKey
@@ -12,6 +14,7 @@ import utils
 MINING_DIFFICULTY: int = 3
 MINIG_SENDER = 'THE BLOCKCHAIN'
 MINING_REWARD = 1.0
+MINIG_TMER_SEC = 20
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 logger = logging.getLogger(__name__)
@@ -19,12 +22,13 @@ logger = logging.getLogger(__name__)
 
 class BlockChain(object):
 
-    def __init__(self, blockchain_address=None, port = None):
+    def __init__(self, blockchain_address=None, port=None):
         self.transaction_pool = []
         self.chain = []
         self.create_block(0, self.hash({}))
         self.blockchain_address = blockchain_address
         self.port = port
+        self.mining_semaphore = threading.Semaphore(1)
 
     def create_block(self, nonce, previous_hash):
         block = utils.sorted_dict_by_key({
@@ -55,8 +59,7 @@ class BlockChain(object):
             return True
 
         if self.verify_transaction_signature(
-            sender_public_key, signature, transaction):
-
+                sender_public_key, signature, transaction):
             # if self.calculate_total_amount(sender_blockchain_address) < float(value):
             #     logger.error({'action': 'add_transaction', 'error': 'no_value'})
             #     return False
@@ -66,7 +69,7 @@ class BlockChain(object):
         return False
 
     def create_transaction(self, sender_blockchain_address,
-                           recipient_blockchain_address,value,
+                           recipient_blockchain_address, value,
                            sender_public_key, signature):
         is_transacted = self.add_transaction(
             sender_blockchain_address, recipient_blockchain_address,
@@ -75,8 +78,7 @@ class BlockChain(object):
 
         return is_transacted
 
-
-    def verify_transaction_signature( self, sender_public_key, signature, transaction):
+    def verify_transaction_signature(self, sender_public_key, signature, transaction):
         sha256 = hashlib.sha256()
         sha256.update(str(transaction).encode('utf-8'))
         message = sha256.digest()
@@ -105,15 +107,29 @@ class BlockChain(object):
         return nonce
 
     def mining(self):
+        if not self.transaction_pool:
+            return False
+
         self.add_transaction(
             sender_blockchain_address=MINIG_SENDER,
             recipient_blockchain_address=self.blockchain_address,
             value=MINING_REWARD)
         nonce = self.proof_of_work()
-        previous_hash =self.hash(self.chain[-1])
+        previous_hash = self.hash(self.chain[-1])
         self.create_block(nonce, previous_hash)
         logger.info({'action': 'mining', 'status': 'success'})
         return True
+
+    def start_mining(self):
+        is_acquire = self.mining_semaphore.acquire(blocking=False)
+        if is_acquire:
+            with contextlib.ExitStack() as stack:
+                stack.callback(self.mining_semaphore.release)
+                self.mining()
+                loop = threading.Timer(MINIG_TMER_SEC, self.start_mining)
+                loop.start()
+
+
 
     def calculate_total_amount(self, blockchain_address):
         total_amount = 0.0
